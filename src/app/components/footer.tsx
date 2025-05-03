649b45a005c75b51c2dafd521cfb51145e19f546
Caio Vellani <caiovellani@outlook.com>
2025-05-03 11:38:21 -0300

    feat(footer): add LinkedIn profile link and enhance author name styling
    - Add Next.js Link component to make author name clickable
    - Link to author's LinkedIn profile
    - Apply strong tag to emphasize author name
    - Import Next.js Link component

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,7 +1,17 @@
+import Link from 'next/link'
+
 export function Footer() {
   return (
     <div className="w-full border-t flex flex-col border-white/20 mt-12 pt-6 text-center text-sm text-zinc-50">
-      <span>Feito por Caio Vellani</span>
+      <Link
+        href={'https://www.linkedin.com/in/caiovborges/'}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span>
+          Feito por <strong>Caio Vellani</strong>
+        </span>
+      </Link>
       <span className="mt-2">Todos os direitos reservados © 2025</span>
     </div>
   )
